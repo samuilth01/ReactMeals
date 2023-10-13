2804a4c64f7619d1e27c985e5258ab5698203d70
samuilth01 <102425504+samuilth01@users.noreply.github.com>
2023-10-13 20:18:06 +0300

    Working with Refs & Forward Refs

--- ReactMeals/src/components/Meals/MealItem/MealItemForm.js
+++ ReactMeals/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,30 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const amountInputRef = useRef();
+
+  const submitHandler = event => {
+     event.preventDefault();
+
+     const enterdAmound = amountInputRef.current.value;
+     const enterdAmoundNumber = +enterdAmound;
+
+     if (enterdAmound.trim().length === 0 || enterdAmoundNumber < 1 || enterdAmoundNumber > 5) {
+      setAmountIsValid(false);
+      return;
+     }
+
+     props.onAddToCard(enterdAmoundNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label='Amount'
         input={{
           id: 'amount_' + props.id,
@@ -16,6 +36,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amaun (1-5).</p>}
     </form>
   );
 };
